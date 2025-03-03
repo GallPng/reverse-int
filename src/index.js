c05aa3474517e381cd250a5741838fc9f4a663a8
Gall <galina.skorobogatova.tui@gmail.com>
2025-03-03 16:48:26 +0700

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = n.toString();
+    if (str[0] !== "-") {
+        let strToM = str.split("");
+        return +strToM.reverse().join("");
+    } else if (str[0] === "-") {
+        let strToM = str.split("");
+        let strToMa = strToM.reverse().join("");
+        return +strToMa.slice(0, -1);
+    }
+};
